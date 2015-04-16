2e3dc3634c45a5ab4643f8c219b03bf2aa557e48
caiopenachini <penachinicaio@hotmail.com>
2015-04-16 18:47:29 +0200

    Initial commit

---
+++ app/src/main/java/app/cheerup/Home.java
@@ -0,0 +1,14 @@
+package app.cheerup;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by NOVO on 15/04/2015.
+ */
+public class Home extends ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
